9d51f674b0882ea2f4c5886dee63040daaa9400a
Krystal <krystalmorel@gmail.com>
2025-03-24 20:06:04 -0400

    second

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div>
-      <h1>Storeit- The only storage solution you need.</h1>
+      <h1 className="text-3xl text-brand-100">Storeit- The only storage solution you need.</h1>
     </div>
   );
 }
