fe3dab9e8a8e9a09743478a8d4f25d138fa9c1f1
Krystal <krystalmorel@gmail.com>
2025-03-26 19:34:23 -0400

    third

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,9 +1,5 @@
-import React from "react";
+import AuthForm from "@/components/AuthForm";
 
-const SignUp = () => {
-    return (
-        <div>Sign Up</div>
-    )
-};
+const SignUp = () => <AuthForm type="sign-in" />
 
 export default SignUp;
---
+++ components/AuthForm.tsx
@@ -0,0 +1,8 @@
+
+type Formtype = "sign-in" | "sign-up";
+
+const AuthForm = ( { type }: { type: Formtype }) => {
+
+}
+
+export default AuthForm;
