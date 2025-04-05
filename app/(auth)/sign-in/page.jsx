537a090c3a8197a053c5a48d1d18c8ced6d258b3
Krystal <krystalmorel@gmail.com>
2025-04-04 21:35:33 -0400

    fifth

--- app/(auth)/sign-in/page.jsx
+++ app/(auth)/sign-in/page.jsx
@@ -1,5 +1,9 @@
 import AuthForm from "@/components/AuthForm";
 
-const SignIn = () => <AuthForm type="sign-in" />
+const SignIn = () => {
+<>
+<AuthForm  type="sign-in"/>
+ </>}
+
 
 export default SignIn;
---
+++ app/(root)/page.jsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+
+export default function Home() {
+  return (
+    <div className= "flex-center">
+      <h1 className="">Storeit- The only storage solution you need.</h1>
+    </div>
+  );
+}
